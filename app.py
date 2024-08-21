9b6363bff11777808f174a41db1f404dda8a2866
Vino k <144210830+K-vino@users.noreply.github.com>
2024-08-21 20:50:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title('WECOME TO MY AI WORLD')
+st.logo("linked.png")
+st.header('I am Vino k')
+st.header('"Try my VMD AI..."')
+
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyApxFggGSBX7ZrUukJFR4_AMpZj2SfmE6U")
+text = st.text_input("Enter you question")
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+if st.button("CLICK HERE"):
+    response = chat.send_message(text)
+    st.write(response.text)
+    
+ 
